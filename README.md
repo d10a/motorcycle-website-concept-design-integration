40724217e9674618a794f09b6bbfa0b743534e66
David GASSAMA <david.gassama@orange.com>
2022-05-28 13:02:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vue 3 + Vite
+
+## Maquettes
+
+- https://dribbble.com/shots/14324106-Montblanc-Motorcycle-website-concept-design
+- https://www.figma.com/file/RDB3WrQRhBH5BKEQGtklSM/Montblanc---Motorcycle-website-concept-design?node-id=4%3A2
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style>
+
+html {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  -webkit-box-sizing: inherit;
+  -moz-box-sizing: inherit;
+  box-sizing: inherit;
+}
+body{
+  min-height: 100vh;
+  background: #060507;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+html, body{
+  font-size: 16px;
+}
+</style>
